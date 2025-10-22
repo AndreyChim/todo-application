645f593382af62004a00a35e60e59bfbc3d134c3
AndreyChim <chimitcyrenov@mail.ru>
2025-10-22 14:40:21 +0800

    Authorize end points & fix logout action

--- app/controllers/api/v1/todo_items_controller.rb
+++ app/controllers/api/v1/todo_items_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::TodoItemsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_todo_item, only: %i[show edit update destroy]
+
     def index
-      @todo_items = TodoItem.all
+      @todo_items = current_user.todo_items.all
+    end
+
+    def edit
     end
   
     def show
@@ -20,6 +26,16 @@ class Api::V1::TodoItemsController < ApplicationController
   
     def set_todo_item
       @todo_item = TodoItem.find(params[:id])
+    end
+
+    def authorized?
+      @todo_item.user == current_user
+    end
+
+    def handle_unauthorized
+      unless authorized?
+        respond_to { |format| format.json { render :unauthorized, status: 401 } }
+      end
     end
   end
   
