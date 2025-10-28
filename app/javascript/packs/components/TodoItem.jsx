886fd27dfbac50a7ecd15f97c86b01c266aa21d8
AndreyChim <chimitcyrenov@mail.ru>
2025-10-28 17:56:57 +0800

    Add debounce function

--- app/javascript/packs/components/TodoItem.jsx
+++ app/javascript/packs/components/TodoItem.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import _ from "lodash";
 import axios from "axios";
 import setAxiosHeaders from "./AxiosHeaders";
 
@@ -21,11 +22,13 @@ class TodoItem extends React.Component {
   }
 
   handleChange() {
+    this.setState({
+      complete: this.completedRef.current.checked,
+    });
     this.updateTodoItem();
   }
   
-  updateTodoItem() {
-    this.setState({ complete: this.completedRef.current.checked });
+  updateTodoItem = _.debounce(() => {
     setAxiosHeaders();
     axios
       .put(this.path, {
@@ -38,7 +41,7 @@ class TodoItem extends React.Component {
       .catch((error) => {
         console.log(error);
       });
-  }
+  }, 1000);
   
   handleDestroy() {
     setAxiosHeaders();
